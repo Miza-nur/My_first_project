e548f8db62c1c2d6d026a9276c92dc9131f5074f
mizan <mizanurrahmanmizan934@gmail.com>
2024-10-09 19:56:29 +0600

    links are added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['*'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: ['Poppins', 'sans-serif'],
+    },
+    colours: {
+      'bg-color':'#091420',
+      'bg-dark-color':'#0cla28',
+      'primmary-color':'#4fe98c'
+    },
+    width:{
+      'custom-image-width':'200px',
+    },
   },
   plugins: [],
 }
